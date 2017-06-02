c5aa33ada00025ff5a08b8bdf5d81cf3840b0c92
meakitfed <nathan.bonnard@hotmail.fr>
2017-06-02 15:35:49 +0200

    Add files via upload

---
+++ ProjetIHMJAVA/src/classes/Flight.java
@@ -0,0 +1,48 @@
+package classes;
+
+public class Flight 
+{
+	private Plane plane;
+	private Airport arrival;
+	private Airport departure;
+	private String company;
+	private String id;
+	
+	public Flight(String id, Airport a1, Airport a2, String company, String model) 
+	{
+		this.id = id;
+		this.departure = a1;
+		this.arrival = a2;
+		this.company = company;
+		this.plane = new Plane(model);
+	}
+
+	@Override
+	public String toString()
+	{
+		String str = "";
+		
+		str+= "vol numéro "+id+" au bord d'un " + plane.getModel() +" appartenant a la compagnie "+company+" qui va de : "+ departure.getShortName() +" à "+arrival.getShortName(); 
+		return str;
+	}
+
+	/**
+	 * @return the arrival
+	 */
+	public Airport getArrival() 
+	{
+		return arrival;
+	}
+
+	/**
+	 * @return the departure
+	 */
+	public Airport getDeparture() 
+	{
+		return departure;
+	}
+	
+
+	
+
+}
---
+++ ProjetIHMJAVA/src/classes/Plane.java
@@ -0,0 +1,27 @@
+package classes;
+
+public class Plane 
+{
+	private String model;
+	private float height;
+	private float speed;
+	private Geolocation geolocation;
+	
+	public Plane(String model) 
+	{
+		this.model = model;
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @return the model
+	 */
+	public String getModel() 
+	{
+		return model;
+	}
+	
+	
+	
+
+}
